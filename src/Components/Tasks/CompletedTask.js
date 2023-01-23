b01a166fd421116a35b2c0aad82afe7fb7e30e4c
aastha-attentive <aastha@attentive.ai>
2023-01-23 19:29:13 +0530

    All Compeleted task

---
+++ src/Components/Tasks/CompletedTask.js
@@ -0,0 +1,32 @@
+import { removeTask } from '../service/localStorage';
+import { getListTask } from '../service/localStorage';
+import swal from 'sweetalert';
+
+const CompTask = ({task,setTaskData,setTaskid}) => {
+
+  function deleteTask(id){
+    swal({
+      title: `Delete ${task.taskname}`,
+      buttons: true,
+      dangerMode: true,
+    })
+    .then((willDelete) => {
+      if (willDelete) {
+        removeTask(id);
+        setTaskData(getListTask());
+      }
+    });
+  }
+  
+  return (
+            <div className="task">
+                    <p className="todo-h">{task.taskname}</p>
+                    <div className="todo-btn">
+                        <i className="fa fa-edit add-btn" title="Edit Item" onClick={()=> setTaskid(task.id)}></i>
+                        <i className="fa fa-trash-o add-btn" title="Delete Item" onClick={()=>deleteTask(task.id)}></i>
+                    </div>    
+            </div>
+  )
+}
+
+export default CompTask;
